86be929397af907afa150c975fb8f67cebf54c70
Markchintu <kintumark95@gmail.com>
2023-02-22 23:27:32 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the size of the number
+ * @n: The number onto which checking is done
+ * Return: 1 for positive number , -1 for anyhting else
+ */
+int print_sign(int n)
+
+{
+	if( n > 0)
+	{
+	_putchar(43);
+	return(1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
